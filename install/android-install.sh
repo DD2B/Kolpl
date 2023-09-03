d16e2ee1a92ea08abcbba415ed1c3b2ed09bac6d
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-09-04 01:44:08 +0300

    Create android-install.sh

---
+++ install/android-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Python packages using pip3
+pip3 install telethon requests python-telegram-bot
+# Download the Python script
+curl "https://raw.githubusercontent.com/kekoDev/kolpl/main/main.py" > "main.py"
+
+reset
+# Run the Python script
+python3 main.py
+
