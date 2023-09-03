acca7833451306cb2a37e18b70f4082e36c256a8
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-09-04 01:46:58 +0300

    Update android-install.sh

--- install/android-install.sh
+++ install/android-install.sh
@@ -3,9 +3,9 @@
 # Install Python packages using pip3
 pip3 install telethon requests python-telegram-bot
 # Download the Python script
-curl "https://raw.githubusercontent.com/kekoDev/kolpl/main/main.py" > "main.py"
+curl "https://raw.githubusercontent.com/kekoDev/kolpl/main/bot.py" > "bot.py"
 
 reset
 # Run the Python script
-python3 main.py
+python3 bot.py
 
